e3fc8760cfa8c080b3da135526c1f3c91051217d
ahmad_maulana <93983578+LanaMaou@users.noreply.github.com>
2023-11-01 17:30:09 +0800

    memperbaiki port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // gunakan ejs
 app.set("view engine", "ejs");
